46d1f04ad79a2bd5be9555d649ea0a0b2b8db089
froilene villaluz <froilene_villaluz@dlsu.edu.ph>
2026-07-30 01:11:08 +0800

    Added algebraic computation methods for all Bernoulli variables

--- src/main/java/ph/edu/dlsu/lbycpob/formulamenu/Bernoulli.java
+++ src/main/java/ph/edu/dlsu/lbycpob/formulamenu/Bernoulli.java
@@ -1,8 +1,6 @@
 package ph.edu.dlsu.lbycpob.formulamenu;
 
 public class Bernoulli implements IFormula{
-    public Bernoulli() {
-    }
 
     private final String[] parameterList = {
             "P1",
@@ -23,6 +21,54 @@ public class Bernoulli implements IFormula{
     private double h2;
 
     private final double g = 9.81;
+
+    public Bernoulli() {
+    }
+
+    // MATH COMPUTATION METHODS
+
+    public void computeP1() {
+        p1 = p2
+                + 0.5 * density * v2 * v2
+                + density * g * h2
+                - 0.5 * density * v1 * v1
+                - density * g * h1;
+    }
+
+    public void computeP2() {
+        p2 = p1
+                + 0.5 * density * v1 * v1
+                + density * g * h1
+                - 0.5 * density * v2 * v2
+                - density * g * h2;
+    }
+
+    public void computeDensity() {
+        double denominator = 0.5 * (v2 * v2 - v1 * v1) + g * (h2 - h1);
+        if (denominator != 0) {
+            density = (p1 - p2) / denominator;
+        } else {
+            density = 0;
+        }
+    }
+
+    public void computeV1() {
+        double valueInsideSqrt = (2 * (p2 - p1) / density) + (v2 * v2) + (2 * g * (h2 - h1));
+        v1 = Math.sqrt(Math.max(0, valueInsideSqrt));
+    }
+
+    public void computeV2() {
+        double valueInsideSqrt = (2 * (p1 - p2) / density) + (v1 * v1) + (2 * g * (h1 - h2));
+        v2 = Math.sqrt(Math.max(0, valueInsideSqrt));
+    }
+
+    public void computeH1() {
+        h1 = ((p2 - p1) / (density * g)) + ((v2 * v2 - v1 * v1) / (2 * g)) + h2;
+    }
+
+    public void computeH2() {
+        h2 = ((p1 - p2) / (density * g)) + ((v1 * v1 - v2 * v2) / (2 * g)) + h1;
+    }
 
     @Override
     public double compute(String variable, String[] values) {
