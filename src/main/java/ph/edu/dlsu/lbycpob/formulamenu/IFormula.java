559f593987e791d9f123aadbeb0a9e650c1a1be7
caeljandcristobal <ccaeljan@gmail.com>
2026-07-29 20:15:08 +0800

    Added an abstract method in the IFormula interface to handle different number of inputs

--- src/main/java/ph/edu/dlsu/lbycpob/formulamenu/IFormula.java
+++ src/main/java/ph/edu/dlsu/lbycpob/formulamenu/IFormula.java
@@ -1,9 +1,7 @@
 package ph.edu.dlsu.lbycpob.formulamenu;
 
 public interface IFormula {
-
     public double compute(String variable, String [] values);
-
     public String[] getParameterList();
-
+    public String[] getInputParameters(String variable);
 }
