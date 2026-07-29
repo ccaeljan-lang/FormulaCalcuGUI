2fdae592a483a6f07a467e146990f05721f5fdea
froilene villaluz <froilene_villaluz@dlsu.edu.ph>
2026-07-30 00:47:57 +0800

    Added a parameterList for Bernoulli

--- src/main/java/ph/edu/dlsu/lbycpob/formulamenu/Bernoulli.java
+++ src/main/java/ph/edu/dlsu/lbycpob/formulamenu/Bernoulli.java
@@ -2,8 +2,17 @@ package ph.edu.dlsu.lbycpob.formulamenu;
 
 public class Bernoulli implements IFormula{
     public Bernoulli() {
-
     }
+
+    private final String[] parameterList = {
+            "P1",
+            "P2",
+            "Density",
+            "V1",
+            "V2",
+            "H1",
+            "H2"
+    };
 
     @Override
     public double compute(String variable, String[] values) {
